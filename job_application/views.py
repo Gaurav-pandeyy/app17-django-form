d8f73603b61b4e37bf7abe7fa5e4528d20142b20
Gaurav-pandeyy <gauravpande568@gmail.com>
2024-11-01 10:14:45 +0530

    Semi Final Commit

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 def index(request):
     if request.method == "POST":
@@ -15,5 +16,11 @@ def index(request):
 
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
+
+
+            message_body = f"{first_name} Thank You for applying for this Job Application."
+            email_message = EmailMessage("Form Submission Confirmation",message_body,to=[email])
+            email_message.send()
             messages.success(request,"Form Submitted SuccessFully")
+
     return render(request, "index.html")
